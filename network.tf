86fe3fd2f73c92765967e7fcc4f8e2b33ab0391f
Christian Gélinas <christian@famillegelinas.com>
2021-02-09 22:05:32 -0500

    phase1

--- network.tf
+++ network.tf
@@ -1,26 +1,26 @@
-resource "google_compute_network" "cr460demo" {
-  name                    = "cr460demo"
+resource "google_compute_network" "devoir1" {
+  name                    = "devoir1"
   auto_create_subnetworks = "false"
 }
 
 
-resource "google_compute_subnetwork" "mtl-dmz" {
-  name          = "mtl-dmz"
+resource "google_compute_subnetwork" "prod-interne" {
+  name          = "prod-interne"
   ip_cidr_range = "172.16.1.0/24"
   region        = "us-east1"
   network       = google_compute_network.cr460demo.self_link
 }
 
-resource "google_compute_subnetwork" "mtl-workload" {
-  name          = "mtl-workload"
-  ip_cidr_range = "10.0.1.0/24"
+resource "google_compute_subnetwork" "prod-dmz" {
+  name          = "prod-dmz"
+  ip_cidr_range = "192.168.42.0/24"
   network       = google_compute_network.cr460demo.self_link
   region        = "us-east1"
 }
 
-resource "google_compute_subnetwork" "mtl-internal" {
-  name          = "mtl-internal"
-  ip_cidr_range = "10.0.2.0/24"
+resource "google_compute_subnetwork" "prod-traitement" {
+  name          = "prod-traitement"
+  ip_cidr_range = "10.0.46.0/24"
   region        = "us-east1"
   network       = google_compute_network.cr460demo.self_link
 }
--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  project = "zonedetest"
+  project = "cr460-devoir1-2054204"
   credentials = "account.json"
   region  = "us-east1"
   zone    = "us-east1-c"
